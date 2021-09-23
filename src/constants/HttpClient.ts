44a58740e2fb90cdc2cca664c2d3a865b816c548
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2021-09-23 13:11:26 -0300

    updated issues

--- src/constants/HttpClient.ts
+++ src/constants/HttpClient.ts
@@ -3,6 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 const HttpClient = () => {
     return axios.create({
         baseURL: 'https://digitown-backend.herokuapp.com',
+        // baseURL: 'http://192.168.15.14:3000',
         timeout: 3000
     })
 }
